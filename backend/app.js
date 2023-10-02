197d568a6794066e1568563f2daf6a1397d5dfbb
Shakhlyn <shakhlyn.sh.du@gmail.com>
2023-10-02 15:01:53 +0600

    Implement a)rate limiting, b) set security HTTP headers, c) Data sanitization, and d) prevent parameter pollution functionality

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
 
 import AppError from "./utils/appError.js";
 import globalErrorHandler from "./controllers/errorController.js";
@@ -10,17 +15,65 @@ import reviewRouter from "./routes/reviewRoutes.js";
 
 const app = express();
 
-// Mddleware:
+// Global Mddlewares:
+
+// set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same IP
+const apiLimiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from your IP. Please try again in an hour",
+});
+
+const loginLimiter = rateLimit({
+  max: 5,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many log-in from your IP. Please try again in an hour",
+});
+
+app.use("/api", apiLimiter);
+app.use("/api/v1/users/login", loginLimiter);
+
 // Use cookie-parser middleware
 app.use(cookieParser());
 
 // body-parser
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
-app.use((req, res, next) => {
-  // console.log(req.headers);
-  next();
-});
+// Data sanitization against NoSQL query injection
+app.use(ExpressMongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "category",
+      "authors",
+      "language",
+      "translated",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "price",
+      "discount",
+      "publisher",
+      "genre",
+      "bestSeller",
+      "condition",
+      "role",
+    ],
+  })
+);
+
+// testing middleware:
+// app.use((req, res, next) => {
+//   // console.log(req.headers);
+//   next();
+// });
 
 // Router Middleware
 app.use("/api/v1/books", bookRouter);
